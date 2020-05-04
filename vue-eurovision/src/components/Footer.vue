c2bef2958339652468f8f27f438cb1fb30b7f9d6
Mayte <may.eater@gmail.com>
2020-05-04 19:30:39 +0200

    component header and footer

---
+++ vue-eurovision/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>© 2020, Mayte &amp; Andrea</p>
+    <p>
+      Images &amp; Data obtained in
+      <a
+        href="https://es.wikipedia.org/wiki/Festival_de_la_Canci%C3%B3n_de_Eurovisi%C3%B3n_2020"
+      >Wikipedia</a>,
+      <a href="https://eurovision.tv/">Eurovision.tv</a>
+    </p>
+  </footer>
+</template>
+
+<style>
+</style>
--- vue-eurovision/src/components/Header.vue
+++ vue-eurovision/src/components/Header.vue
@@ -1,9 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header class="navbar">
+    <router-link class="navbar-brand" to="/">
+      <img src="../assets/EuroFlags/EuroNL.png" width="30" height="30" alt /> Eurovision 2020
+    </router-link>
+    <div class="navbar-nav-scroll">
+      <ul class="navbar-nav bd-navbar-nav flex-row">
+        <li class="nav-item">
+          <router-link to="/about">Festival</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/rotterdam">Rotterdam 2020</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/singers">Participants</router-link>
+        </li>
+        <!-- <li class="nav-item">
+            <router-link to="/listCountryVote">Votaciones</router-link>
+        </li>-->
+
+        <li class="nav-item">
+          <router-link to="/votes">Votes</router-link>
+        </li>
+      </ul>
+    </div>
+    <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+      <li class="nav-item">
+        <a href="https://github.com/missmay4/Eurovision-2020-Project">
+          <i class="fa fa-github fa-lg" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="https://open.spotify.com/playlist/2GAHVbzeg2z6p5roXVayDe?si=XwW_VGJ4Q6-vP6EaMfynog"
+        >
+          <i class="fa fa-spotify fa-lg" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://twitter.com/deathscythe_4">
+          <i class="fa fa-twitter fa-lg" aria-hidden="true"></i>
+        </a>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <style>

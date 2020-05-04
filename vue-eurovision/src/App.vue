e08b0b40294bff3d7d9f39ca47a247af0f2e201c
Mayte <may.eater@gmail.com>
2020-05-04 19:35:20 +0200

    added header and footer components

--- vue-eurovision/src/App.vue
+++ vue-eurovision/src/App.vue
@@ -1,58 +1,26 @@
 <template>
   <div id="app">
-    <header class="navbar">
-      <router-link class="navbar-brand" to="/">
-        <img src="./assets/EuroFlags/EuroNL.png" width="30" height="30" alt /> Eurovision 2020
-      </router-link>
-      <div class="navbar-nav-scroll">
-        <ul class="navbar-nav bd-navbar-nav flex-row">
-          <li class="nav-item">
-            <router-link to="/singers">Participantes</router-link>
-          </li>
-          <!-- <li class="nav-item">
-            <router-link to="/listCountryVote">Votaciones</router-link>
-          </li>-->
-          <li class="nav-item">
-            <router-link to="/votes">Votos</router-link>
-          </li>
-        </ul>
-      </div>
-      <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-        <li class="nav-item">
-          <a href="https://github.com/missmay4/Eurovision-2020-Project">
-            <i class="fa fa-github fa-lg" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            href="https://open.spotify.com/playlist/2GAHVbzeg2z6p5roXVayDe?si=XwW_VGJ4Q6-vP6EaMfynog"
-          >
-            <i class="fa fa-spotify fa-lg" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="https://twitter.com/deathscythe_4">
-            <i class="fa fa-twitter fa-lg" aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-    </header>
+    <Header />
     <div class="margin">
       <router-view />
     </div>
-
-    <footer>
-      <p>© 2020, Mayte &amp; Andrea</p>
-      <p>
-        Images &amp; Data obtained in
-        <a
-          href="https://es.wikipedia.org/wiki/Festival_de_la_Canci%C3%B3n_de_Eurovisi%C3%B3n_2020"
-        >Wikipedia</a>,
-        <a href="https://eurovision.tv/">Eurovision.tv</a>
-      </p>
-    </footer>
+    <Footer />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "main",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
 
 <style>
 :root {

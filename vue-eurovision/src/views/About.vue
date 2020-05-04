6b37bdcb010d28bc33ffdbf91357ad3e8fa50c9d
Mayte <may.eater@gmail.com>
2020-05-04 19:37:12 +0200

    views About and Rotterdam info

--- vue-eurovision/src/views/About.vue
+++ vue-eurovision/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Eurovision</h1>
+    <p>
+      <b>The history of the Eurovision Song Contest began as the brainchild of Marcel Bezençon of the EBU. The Contest was based on Italy's Sanremo Music Festival and was designed to test the limits of live television broadcast technology.</b>
+    </p>
+    <p>The first Contest was held on 24 May 1956, when seven nations participated. With a live orchestra, the norm in the early years, and simple sing-along songs on every radio station, the Contest grew into a true pan-European tradition.</p>
   </div>
 </template>
---
+++ vue-eurovision/src/views/Rotterdam.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>About Rotterdam 2020</h1>
+</template>

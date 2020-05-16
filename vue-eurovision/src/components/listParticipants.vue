ebc83609ca9314658453ce83c630b4d1e695924d
Mayte <may.eater@gmail.com>
2020-05-16 13:50:14 +0200

    deleted nav, get only all participants

--- vue-eurovision/src/components/listParticipants.vue
+++ vue-eurovision/src/components/listParticipants.vue
@@ -1,32 +1,41 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-md-6">
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
+    <h1>Participants</h1>
+    <!-- <div class="row">
+      <div class="col-md-12">
+    <ul class="nav nav-tabs justify-content-center">-->
+    <!-- <li class="nav-item">
             <a class="nav-link" href="#participants" @click="gala=undefined">Ver todos</a>
+    </li>-->
+    <!-- <li class="nav-item">
+            <a class="nav-link" href="#first-semi-final" @click="gala=1">1st Semifinal</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#first-semi-final" @click="gala=1">1ª Semifinal</a>
+            <a class="nav-link" href="#second-semi-final" @click="gala=2">2nd Semifinal</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#second-semi-final" @click="gala=2">2ª Semifinal</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#grand-final" @click="gala=3">Gran Final</a>
+            <a class="nav-link" href="#grand-final" @click="gala=3">Final</a>
           </li>
         </ul>
       </div>
-    </div>
+    </div>-->
     <div class="row no-gutters">
       <Participant
+        v-for="participant in participants"
+        :name="participant.name"
+        :country="participant.country.name"
+        :countryCode="participant.country.code"
+        :songLink="participant.song_link"
+      />
+
+      <!-- <Participant
         v-for="participant in participants"
         v-if="gala===undefined || participant.gala.gala_id===gala"
         :name="participant.name"
         :country="participant.country.name"
         :countryCode="participant.country.code"
         :songLink="participant.song_link"
-      />
+      />-->
     </div>
   </div>
 </template>
@@ -43,12 +52,12 @@ export default {
     };
   },
   mounted() {
-    axios.get("http://localhost:8080/participant/").then(response => {
+    axios.get("http://localhost:8080/onlyparticipants/").then(response => {
       let data = response.data;
       data.sort((a, b) => a.country.name.localeCompare(b.country.name));
       this.participants = data;
     });
-    let currentRoute = this.$router.currentRoute.hash;
+    /* let currentRoute = this.$router.currentRoute.hash;
 
     if (currentRoute === "#first-semi-final") {
       this.gala = 1;
@@ -56,7 +65,7 @@ export default {
       this.gala = 2;
     } else if (currentRoute === "#grand-final") {
       this.gala = 3;
-    }
+    } */
   },
   components: {
     Participant

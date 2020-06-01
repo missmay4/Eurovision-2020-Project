8fed0c4684b560cceb31b5eeb4444d0b4fc6d7b1
Andrea <andreasanchez1016@gmail.com>
2020-06-01 18:12:58 +0200

    UserFavorites and listFavorites components

--- vue-eurovision/src/components/UserFavorites.vue
+++ vue-eurovision/src/components/UserFavorites.vue
@@ -1,22 +1,37 @@
 <template>
-    <h1>Holi</h1>
+    <div class="card border-primary mb-3" style="max-width: 18rem;">
+  <div class="card-header">{{ usuario.username }}</div>
+  <div class="card-body text-primary">
+    <h5 class="card-title">Primary card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 </template>
 <script>
-//import axios from "axios"
+import axios from "axios"
 export default {
    name: "UserFavorites",
-   props:{
-       user: Object,
-       participant: Object,
-       
-   },
+  data() {
+    return {
+      usuario: Object,
+      participants: Object,
+    };
+  },
+  
     methods: {
     loadImage(imageName) {
       return require("../assets/" + imageName);
     },
+      mounted() {
+    axios.get("http://localhost:8080/user_favorites/").then((response) => {
+      this.usuario = response.data;
+      console.log(response.data)
+    });
+  },
   }
    
 }
+
 </script>
 <style scoped>
 
---
+++ vue-eurovision/src/components/listFavorites.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Favorites</h1>
+     <UserFavorites
+          v-for="usuario in usuarios"
+          :usuario="usuario"
+          v-bind:key="usuario.username"
+        />
+</template>
+<script>
+import UserFavorites from "@/components/UserFavorites.vue";
+import axios from "axios";
+export default {
+  name: "listFavorites",
+  data() {
+    return {
+      usuarios: [],
+      participants: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8080/user_favorites/").then((response) => {
+      this.usuarios = response.data;
+      console.log(response.data);
+    });
+  },
+  components: {
+    UserFavorites,
+  },
+};
+</script>
+<style scoped>
+
+</style>

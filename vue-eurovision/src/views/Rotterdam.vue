1cb59870d07db8b9db618a10cada153cd25e4bdf
Andrea <andreasanchez1016@gmail.com>
2020-06-07 16:43:28 +0200

    Rotterdam view fixed

--- vue-eurovision/src/views/Rotterdam.vue
+++ vue-eurovision/src/views/Rotterdam.vue
@@ -3,20 +3,18 @@
     <div class="header">
       <h1 class="title display-4">Rotterdam 2020</h1>
     </div>
-    <div class="row">
+    <div class="row justify-content-center">
       <div class="col" md="6" offset-md="3">
-        <img class=" d-block" src="../assets/Rotterdam/Rotterdam.svg" width="700px" height="350px" />
-      </div>
-      <div class="col left" md="6" offset-md="3">
+        <img class="d-block mx-auto" src="../assets/Rotterdam/Rotterdam.svg" width="700px" height="350px" />
         <p class="par1">
           El Festival de Eurovisión de 2020 iba a ser la edicion número 65 del festival.
-          Esta edición iba a ser celbrada en la ciudad de Rotterdam, Paises Bajos, país de origen
+          Esta edición iba a ser celebrada en la ciudad de Rotterdam, Paises Bajos, país de origen
           de el ganador del la edición de 2019 en Tel Aviv, Israel, Duncan Lurence con su canción "Arcade".
         </p>
         <p>
           Habría sido la quinta vez que Paises Bajos hubiera organizado el festival, cosa que no sucedia desde 1980.
           Iba a celebrarse en el Rotterdam Ahoy.
-          El concurso fue cancelado el 18 de marzo de 2020, debido a la pandemia mundial COVID-19. 
+          El concurso fue cancelado el 18 de marzo de 2020, debido a la pandemia mundial COVID-19.
           Es la primera vez en la historia de Eurovisión que el concurso ha tenido que ser cancelado.
         </p>
       </div>
@@ -28,14 +26,17 @@
     <div class="row">
       <div class="col" md="6" offset-md="3">
         <p>
-          Estaba organizado para ser llevado acabo en dos semifinales, una el 12 y otra el 14 de mayo, y después la gran final el 16 de mayo de 2020.
-          La lista de paises participantes se extendia hata 41, Bulgaria y Ucrania volverían despues de no participar en 
+          Estaba organizado para ser llevado acabo en dos semifinales, una el 12 y otra el 14 de mayo, y después la gran
+          final el 16 de mayo de 2020.
+          La lista de paises participantes se extendia hata 41, Bulgaria y Ucrania volverían despues de no participar en
           la edición de 2019, mientras Hungría y Montenegro habían confirmado que no participarían en esta edición.
           Todos los artistas y canciones fueron confirmados a principios de marzo de 2020.
         </p>
         <p>
-          En lugar de la edición cancelada, la EBU y sus miembros neerlandeses NPO, NOS y AVROTROS organizaron un espectáculo
-          llamado Eurovision: Europe Shine a Light, para mostrar a los paises selecionados para la gran final el 16 de mayo.
+          En lugar de la edición cancelada, la EBU y sus miembros neerlandeses NPO, NOS y AVROTROS organizaron un
+          espectáculo
+          llamado Eurovision: Europe Shine a Light, para mostrar a los paises selecionados para la gran final el 16 de
+          mayo.
           Rotterdam seguirá siendo la ciudad hospedadora para el festival del año que viene.
         </p>
         <img class="mx-auto d-block img-fluid" src="../assets/Rotterdam/shine.jpg" width="700px" />
@@ -66,12 +67,3 @@
     </div>
   </div>
 </template>
-<style>
-  .left {
-    padding-left: 2em !important;
-  }
-
-  .right {
-    margin-right: 1em !important;
-  }
-</style>
